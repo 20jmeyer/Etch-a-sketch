c119a2bf66e574dc459fbb3d6ca7d14293f16a0d
Jake Meyer <20jmeyer@gmail.com>
2022-04-12 16:14:07 -0500

    CSS grid and variable size

--- scripts.js
+++ scripts.js
@@ -1,18 +1,17 @@
-const container = document.querySelector(".container");
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.className = "row";
-    for (let j = 0; j < 16; j++) {
-        const square = document.createElement('div');
-        square.className = "square";
-        square.style.height = "50px";
-        square.style.width = "50px";
-        square.style.backgroundColor = "white";
-        square.style.borderColor = "black";
-        square.style.borderStyle = "solid";
-        row.appendChild(square);
+
+
+
+function makeGrid(size) {
+    const container = document.querySelector("#container");
+    container.style.gridTemplateColumns=`repeat(${size},1fr)`;
+    container.style.gridTemplateRows=`repeat(${size},1fr)`;
+    
+    for (let i = 0; i < (size*size); i++) {
+        let cell = document.createElement("div");
+        container.appendChild(cell).className="grid-item";
     }
-    container.appendChild(row);
+
 }
 
+makeGrid(60);
