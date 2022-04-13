5fa6bf1035acb051de9ea5d47251c2cfd8cb22ae
Jake Meyer <20jmeyer@gmail.com>
2022-04-12 20:06:23 -0500

    Add size and clear inputs

--- scripts.js
+++ scripts.js
@@ -6,8 +6,16 @@ function colorCell(e) {
     mousedown=true;
 }
 
+function changeSize(e){
+    console.log(e.target.value);
+    currentSize=e.target.value;
+    makeGrid(e.target.value);
+}
+
+
 function makeGrid(size) {
     const container = document.querySelector("#container");
+    container.innerHTML='';
     container.style.gridTemplateColumns=`repeat(${size},1fr)`;
     container.style.gridTemplateRows=`repeat(${size},1fr)`;
     
@@ -18,5 +26,22 @@ function makeGrid(size) {
     }
 
 }
+let currentSize=30;
 
-makeGrid(60);
+function clearGrid(){
+    const container = document.querySelector("#container");
+    container.innerHTML='';
+    makeGrid(currentSize);
+}
+
+function init(){
+    const size=document.querySelector("#size");
+    size.addEventListener("input",changeSize);
+    const clear=document.querySelector(".clear");
+    clear.addEventListener("click",clearGrid);
+    makeGrid(currentSize);
+}
+
+
+window.onload=init();
+
